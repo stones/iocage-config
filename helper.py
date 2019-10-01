4cb15c93eff2a91571b645082dd3f6956a380023
Tom Stones <tom@tomstones.com.au>
2019-10-02 02:04:30 +1000

    ADDED | CLI argument parser

--- helper.py
+++ helper.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3.6
-import argparse
+import sys
+# insert at 1, 0 is the script path (or '' in REPL)
+sys.path.insert(1, './lib')
 
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                    help='an integer for the accumulator')
-parser.add_argument('--sum', dest='accumulate', action='store_const',
-                    const=sum, default=max,
-                    help='sum the integers (default: find the max)')
+from parseCliArgs import cliArgs
 
-args = parser.parse_args()
-print( args.accumulate(args.integers) )
+print(cliArgs.file)
