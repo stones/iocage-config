a5926b6d7b206cd5387df60171f969dfa6dde6a6
Tom Stones <tom@tomstones.com.au>
2019-10-08 19:05:47 +1100

    UPDATED | added switch

--- helper.py
+++ helper.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3.6
 import sys
+import subprocess
 sys.path.insert(1, './lib')
 
 from parseCliArgs import cliArgs
@@ -7,10 +8,19 @@ from loadConfig import load
 
 config = load(cliArgs.file)
 
+jailName = 'sonarr'
+
 def handleCopy(params):
     # iocage exec sonarr mkdir /usr/local/etc/rc.d
     # cp ./sonarr.rc  /mnt/iocage/jails/sonarr
     # iocage exec sonarr chmod 555 /usr/local/etc/rc.d/sonarr
+    try:
+        subprocess.check_output(['iocage', 'exec', jailName, 'mkdir','/usr/local/etc/rc.d' ])
+        subprocess.check_output(['cp', params.src, '/root' + params.dest ])
+        subprocess.check_output(['iocage', 'exec', 'sonarr', 'chmod', '555', '/usr/local/etc/rc.d/sonarr' ])
+    except subprocess.CalledProcessError as e:
+        print e.output
+
     print('Copy not implemented:', params)
 
 def handleExec(params):
