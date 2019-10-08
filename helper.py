11e98fe00e83c5039b0416f6abe909771f6100b5
Tom Stones <tom@tomstones.com.au>
2019-10-08 14:32:36 +1100

    ADDED | Task handling

--- helper.py
+++ helper.py
@@ -7,4 +7,22 @@ from loadConfig import load
 
 config = load(cliArgs.file)
 
-print(config['version'])
+def handleUnknownAction( name, config):
+    print('I dont know how to handle:', name)
+
+def handleNameAction(description):
+    print('TASK:', description)
+
+def handleTask(taskName, config):
+    if(taskName == 'name'):
+        handleNameAction(config)
+    else:
+        handleUnknownAction(taskName, config)
+
+def handleTaskGroup( taskGroup):
+    for task in taskGroup :
+        handleTask(task, taskGroup[task])
+
+for taskGroup in config['tasks']:
+    handleTaskGroup( taskGroup ) 
+    print('')
